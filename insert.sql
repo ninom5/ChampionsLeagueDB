fb0edebaa692f6ed8952adb2ad98c9accea58141
ninom5 <nmilas00@fesb.hr>
2024-12-03 15:25:04 +0100

    part of data inserted into db

---
+++ insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO Liga_prvaka
+VALUES 
+('2024', 'Real Madrid');
+
+INSERT INTO Igrac
+VALUES 
+(55555, 'Lionel', 'Messi', 'ST', '2005-07-01', '2021-08-10', 'Barcelona'),
+(77777	, 'Erling',  'Haaland',  'ST',  '2022-07-01',  null,'Manchester City');
+
+
+INSERT INTO Trener
+VALUES 
+(111, 'Carlo', 'Ancelotti', '2021-07-01', null, 'Real Madrid');
+
+INSERT INTO Utakmica
+VALUES (2222, '3:0', 'finale', '2015-06-14', 'Cuneyt Cakir', 'Camp Nou', 'Barcelona', 'Real Madrid');
+
+INSERT INTO Momcad VALUES
+('Real Madrid', 'Santiago bernabeu');
+
+
+INSERT INTO Statistika_igraca VALUES
+(77777, 4444,	0, 0, 0,	1, 0, 80, 2, 5, 2, 0, 1);
+INSERT INTO Statistika_momcadi VALUES
+(2222,	"Barcelona",	3, 15,	2, 10,	2, 7, 1,	3);
